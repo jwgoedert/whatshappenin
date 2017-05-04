80303e2ef8d1920dcd5acccfc7bfacfc05b26438
James Wesley Goedert <jwgoedert@gmail.com>
2017-05-04 18:46:43 -0500

    (test) Add env port to listen.

--- index.js
+++ index.js
@@ -79,5 +79,5 @@ app.get('*', (req, res) => {
 
 // start the server
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
+  console.log(`Server is running on http://localhost:${process.env.PORT} or http://127.0.0.1:${process.env.PORT}`);
 });
